0848bc98fb262f0425358720bae765099fa8c511
Alexander Van der Bellen <alexandervanderbellen@catalyst-au.net>
2025-03-05 12:12:51 +0800

    MDL-80984: Update penalty form JS

--- grade/penalty/duedate/manage_penalty_rule.php
+++ grade/penalty/duedate/manage_penalty_rule.php
@@ -136,6 +136,9 @@ if ($edit) {
         // Redirect to the same page.
         redirect($url, get_string('changessaved'), 0, notification::NOTIFY_SUCCESS);
     }
+
+    // Add javascript to handle the repeater.
+    $PAGE->requires->js_call_amd('gradepenalty_duedate/edit_penalty_form', 'init');
 }
 
 // Start output.
