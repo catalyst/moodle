0f8e3c1664ae4ec11f0707a262af7f7f070a586e
Alexander Van der Bellen <alexandervanderbellen@catalyst-au.net>
2025-06-20 09:51:22 +0800

    MDL-84981 core_grades: Resolve Behat failures

--- public/mod/assign/locallib.php
+++ public/mod/assign/locallib.php
@@ -5550,6 +5550,8 @@ class assign {
                         new \grade_grade([
                             'deductedmark' => $gradebookgrade->deductedmark ?? 0,
                             'overridden' => $gradebookgrade->overridden ?? 0,
+                            'itemid' => $this->get_grade_item()->id,
+                            'userid' => $user->id,
                         ], false)
                     );
 
@@ -5782,6 +5784,8 @@ class assign {
                     new \grade_grade([
                         'deductedmark' => $deductedmark,
                         'overridden' => $userid > 0 ? $this->get_grade_item()->get_grade($userid)->overridden : 0,
+                        'itemid' => $this->get_grade_item()->id,
+                        'userid' => $userid,
                     ], false)
                 );
                 $gradeoutput = $penaltystatus . format_float($penalisedgrade, $this->get_grade_item()->get_decimals());
